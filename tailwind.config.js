63521a24b1d10676741ece8b3a0564dc21fcd30b
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-11-26 15:00:04 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow:{
+        'lg':' 0 0px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
+      }
+    },
+  },
+  plugins: [],
+};
